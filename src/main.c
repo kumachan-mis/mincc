c5f9dc67b12d32468c95e101e751f9330ae5336c
kumachan-mis <y.s11nov@fuji.waseda.jp>
2020-03-20 19:52:39 +0900

    refactor: use recursive descent parsing

--- src/main.c
+++ src/main.c
@@ -7,71 +7,75 @@
 #include "mincc_memory.h"
 
 
-typedef enum {
-    TYPE_INT,
-    TYPE_PLUS,
-    TYPE_MINUS,
-    TYPE_EOF
-} TOKEN_TYPE;
-
 typedef union {
     int value_int;
-} TOKEN_VALUE;
+} Value;
+
+
+typedef enum {
+    TOKEN_INT,
+    TOKEN_PLUS,
+    TOKEN_MINUS,
+    TOKEN_EOF
+} TokenType;
 
 typedef struct {
-    TOKEN_TYPE type;
-    TOKEN_VALUE value;
+    TokenType type;
+    Value value;
 } Token;
 
-Vector* read_tokens(FILE* file_ptr);
+Vector* tokenize(FILE* file_ptr);
 Token* read_token(FILE* file_ptr);
 Token* read_token_int(FILE* file_ptr);
+Token* token_new(TokenType type);
+void tokens_delete(Vector* tokens);
+
+
+typedef enum {
+    AST_INT,
+    AST_ADD,
+    AST_SUB
+} AstType;
+
+typedef struct _Ast {
+    AstType type;
+    Value value;
+    struct _Ast *lhs;
+    struct _Ast *rhs;
+} Ast;
+
+Ast* parse(Vector* tokens);
+Ast* parse_int(Vector* tokens, size_t* pos);
+Ast* ast_new(AstType type);
+Ast* ast_delete(Ast* ast);
+
+
+void print_code(Ast* ast);
+
 
 int main(int argc, char* argv[]) {
     fprintf(stdout, ".global _main\n");
     fprintf(stdout, "_main:\n");
 
-    Vector* tokens = read_tokens(stdin);
-    size_t i = 0;
+    Vector* tokens = tokenize(stdin);
+    Ast* ast = parse(tokens);
+    print_code(ast);
 
-    Token* token = (Token*)vector_at(tokens, i);
-    i++;
-    assert(token->type == TYPE_INT);
-    fprintf(stdout, "\tmov $%d, %%eax\n", token->value.value_int);
-    while (1) {
-        token = (Token*)vector_at(tokens, i);
-        i++;
-        if (token->type == TYPE_EOF) break;
-        switch (token->type) {
-            case TYPE_PLUS:
-                token = (Token*)vector_at(tokens, i);
-                i++;
-                assert(token->type == TYPE_INT);
-                fprintf(stdout, "\tadd $%d, %%eax\n", token->value.value_int);
-                break;
-            case TYPE_MINUS:
-                token = (Token*)vector_at(tokens, i);
-                i++;
-                assert(token->type == TYPE_INT);
-                fprintf(stdout, "\tsub $%d, %%eax\n", token->value.value_int);
-                break;
-            default:
-                fprintf(stderr, "Error: unexpected token\n");
-                exit(1);
-        }
-    
-    }
+    printf("\tpop %%rax\n");
     printf("\tret\n");
-    vector_delete(tokens);
+
+    ast_delete(ast);
+    tokens_delete(tokens);
+
     return 0;
 }
 
-Vector* read_tokens(FILE* file_ptr) {
+Vector* tokenize(FILE* file_ptr) {
     Vector* tokens = vector_new();
     while (1) {
         Token* token = read_token(file_ptr);
         vector_push_back(tokens, token);
-        if (token->type == TYPE_EOF) break;
+        if (token->type == TOKEN_EOF) break;
     }
     return tokens;
 }
@@ -86,14 +90,11 @@ Token* read_token(FILE* file_ptr) {
     Token* token = (Token*)safe_malloc(sizeof(Token));
     switch (c) {
         case '+':
-            token->type = TYPE_PLUS;
-            break;
+            return token_new(TOKEN_PLUS);
         case '-':
-            token->type = TYPE_MINUS;
-            break;
+            return token_new(TOKEN_MINUS);
         default:
-            token->type = TYPE_EOF;
-            break;
+            return token_new(TOKEN_EOF);
     }
     return token;
 }
@@ -108,8 +109,97 @@ Token* read_token_int(FILE* file_ptr) {
         }
         value = 10 * value + (c - '0');
     }
-    Token* token = (Token*)safe_malloc(sizeof(Token));
-    token->type = TYPE_INT;
+    Token* token = token_new(TOKEN_INT);
     token->value.value_int = value;
     return token;
+}
+
+Token* token_new(TokenType type) {
+    Token* token = (Token*)safe_malloc(sizeof(Token));
+    token->type = type;
+    return token;
+}
+
+void tokens_delete(Vector* tokens) {
+    vector_delete(tokens);
+}
+
+Ast* parse(Vector* tokens) {
+    size_t pos = 0;
+    Ast* ast = parse_int(tokens, &pos);
+    Ast* lhs = NULL;
+    Ast* rhs = NULL;
+
+    while (1) {
+        Token* token = (Token*)vector_at(tokens, pos);
+        pos++;
+        switch (token->type) {
+            case TOKEN_PLUS:
+                lhs = ast;
+                rhs = parse_int(tokens, &pos);
+                ast = ast_new(AST_ADD);
+                ast->lhs = lhs;
+                ast->rhs = rhs;
+                break;
+            case TOKEN_MINUS:
+                lhs = ast;
+                rhs = parse_int(tokens, &pos);
+                ast = ast_new(AST_SUB);
+                ast->lhs = lhs;
+                ast->rhs = rhs;
+                break;
+            default:
+                return ast;
+        }
+    }
+}
+
+Ast* parse_int(Vector* tokens, size_t* pos) {
+    Ast* ast = ast_new(AST_INT);
+    Token* token = (Token*)vector_at(tokens, *pos);
+    (*pos)++;
+    assert(token->type == TOKEN_INT);
+    ast->value.value_int = token->value.value_int;
+    return ast;
+}
+
+Ast* ast_new(AstType type) {
+    Ast* ast = (Ast*)safe_malloc(sizeof(Ast));
+    ast->type = type;
+    ast->lhs = NULL;
+    ast->rhs = NULL;
+    return ast;
+}
+
+Ast* ast_delete(Ast* ast) {
+    if (ast->lhs != NULL) ast_delete(ast->lhs);
+    if (ast->rhs != NULL) ast_delete(ast->rhs);
+    free(ast);
+}
+
+void print_code(Ast* ast) {
+    if (ast == NULL) return;
+
+    print_code(ast->lhs);
+    print_code(ast->rhs);
+
+    switch (ast->type) {
+        case AST_ADD:
+            fprintf(stdout, "\tpop %%rdi\n");
+            fprintf(stdout, "\tpop %%rax\n");
+            fprintf(stdout, "\tadd %%edi, %%eax\n");
+            fprintf(stdout, "\tpush %%rax\n");
+            break;
+        case AST_SUB:
+            fprintf(stdout, "\tpop %%rdi\n");
+            fprintf(stdout, "\tpop %%rax\n");
+            fprintf(stdout, "\tsub %%edi, %%eax\n");
+            fprintf(stdout, "\tpush %%rax\n");
+            break;
+        case AST_INT:
+            fprintf(stdout, "\tpush $%d\n", ast->value.value_int);
+            break;
+        default:
+            exit(1);
+    }
 }
