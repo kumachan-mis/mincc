d12137e8d5ce998d2323b60b72ec24425fd34824
kumachan-mis <y.s11nov@fuji.waseda.jp>
2020-03-22 00:42:35 +0900

    feat: Allow expressions with logical operators

--- test/test.sh
+++ test/test.sh
@@ -29,9 +29,9 @@ test_mincc "1-6+10"            5
 test_mincc "2+110-92"         20
 test_mincc "1000-990+121+92" 223
 
-test_mincc "1*2 + 5*8"    42
-test_mincc "5*6 - 10/2"   25
-test_mincc "100/3/3"    11
+test_mincc "1*2 + 5*8"     42
+test_mincc "5*6 - 10/2"    25
+test_mincc "100/3/3"       11
 test_mincc "100%7 + 22%6 "  6
 
 test_mincc "(1+2) * 3"              9
@@ -86,5 +86,21 @@ test_mincc "1^1 | 1^1"  0
 test_mincc "0&1 | 1&1"  1
 test_mincc "0&1 | 1&0"  0
 test_mincc "1&1 | 1&1"  1
+test_mincc "~2 & 7"     5
+
+test_mincc "12 == 12"         1
+test_mincc "12 == 19"         0
+test_mincc "12 != 12"         0
+test_mincc "12 != 19"         1
+test_mincc "1 == 1 && 2 == 2" 1
+test_mincc "1 != 1 && 2 == 2" 0
+test_mincc "1 == 1 && 2 != 2" 0
+test_mincc "1 != 1 && 2 != 2" 0
+test_mincc "1 == 1 || 2 == 2" 1
+test_mincc "1 != 1 || 2 == 2" 1
+test_mincc "1 == 1 || 2 != 2" 1
+test_mincc "1 != 1 || 2 != 2" 0
+test_mincc "!0"               1
+test_mincc "!1"               0
 
 echo "OK"
