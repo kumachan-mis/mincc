fa8a3cc1539812e342829ec9c57efcc6244ddd5c
Kumachan <y.s11nov@fuji.waseda.jp>
2020-03-28 21:59:51 +0900

    test: refine test script (#46)

--- test/testlib.c
+++ test/testlib.c
@@ -1,6 +1,3 @@
-int printf(const char * restrict format, ...);
-
-
 int one() {
     return 1;
 }
@@ -9,14 +6,15 @@ int five() {
     return 5;
 }
 
-int sum(int x1, int x2, int x3, int x4, int x5, int x6) {
-    return x1 + x2 + x3 + x4 + x5 + x6;
-}
-
 int square(int x) {
     return x*x;
 }
 
+int sum(int x1, int x2, int x3, int x4, int x5, int x6) {
+    return x1 + x2 + x3 + x4 + x5 + x6;
+}
+
+int printf(const char * restrict format, ...);
 int put_int(int x) {
     printf("%d\n", x);
     return x;
